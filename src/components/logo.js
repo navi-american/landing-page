1300d8aee91343806ca2623d6254b5e50d54d0cb
Felipe Souza <felipe.souza@flashapp.com.br>
2023-03-01 16:33:15 -0300

    alter title

--- src/components/logo.js
+++ src/components/logo.js
@@ -12,7 +12,7 @@ export default function Logo() {
         variant: 'links.logo',
       }}
     >
-      <Image src={logo} sx={{ display: 'flex' }} alt="startup landing logo" />
+      <Image src={logo} sx={{ display: 'flex' }} alt="navi american logo" />
     </Link>
   );
 }
